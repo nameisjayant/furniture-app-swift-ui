8d8ecd6a76b44df1c91fb924ca6caff05e4a4861
nameisjayant <jayntkumar99@gmail.com>
2023-01-31 18:09:57 +0530

    Initial Commit

---
+++ Furniture App/Furniture_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Furniture_AppApp.swift
+//  Furniture App
+//
+//  Created by jayant kumar on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Furniture_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
